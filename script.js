2592196b762230b8b77aa281bcf08765f1b7af20
LATEEF AHMAD BABA <91480408+Mohdlateef@users.noreply.github.com>
2023-11-24 16:08:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ let str;
 for(let i=0;i<20;i++)
 	{if(i%3==0&&i%5==0)
 	{
-		str=str+
-		// alert("FizzBuzz");
+		// str=str+
+		 alert("FizzBuzz");
 	}
 		else if(i%3==0)
 		{
-			// alert("Fizz");
+			alert("Fizz");
 		}
 			else if(i%5==0)
 			{
-				// alert("Buzz")
+				alert("Buzz")
 			}
 				
 		
